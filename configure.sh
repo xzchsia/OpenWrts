6e920a8a4a8b49b1d2986228cc7db152d9cff950
xzchsia <xzchsia@outlook.com>
2025-02-15 05:34:33 +0000

    依据最新的代码路径，修改默认IP为192.168.10.1

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.
 
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/luci2/bin/config_generate
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
